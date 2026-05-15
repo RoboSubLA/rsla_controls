ed530263c8250067694fc9dd219781594459196a
Mark Raspopov <63547351+coderMassive@users.noreply.github.com>
2026-05-14 17:08:00 -0700

    feat: ros integration for dvl and ahrs (#2)
    * ros integration
    
    * added back baro
    
    * fixed some small unnecessary changes according to pull request comments
    
    * feat: add colcon.meta and implement x,y controls
    
    * refactor: odd whitespace
    
    ---------
    
    Co-authored-by: Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,11 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <Servo.h>
+#include <math.h>
 
 #include <MS5837.h>
-
 #include <ADS1X15.h>
 
 #include <LibAlg.hpp>
@@ -27,6 +26,9 @@
 #include <std_msgs/msg/int32.h>
 #include <std_msgs/msg/float32.h>
 
+#include <geometry_msgs/msg/point.h>
+#include <geometry_msgs/msg/quaternion.h>
+
 #include <rsla_interfaces/msg/euler_angles.h>
 #include <rsla_interfaces/msg/pose_euler.h>
 #include <rsla_interfaces/msg/pid.h>
@@ -38,7 +40,7 @@
   static volatile int64_t init = -1; \
   if (init == -1) { init = uxr_millis();} \
   if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
-} while(0)\
+} while(0)
 
 //#define DEBUG
 
@@ -82,7 +84,9 @@ bool startup_successful = true;
 bool vehicle_armed = false;
 
 float x = 0;
+float x_setpoint = 0;
 float y = 0;
+float y_setpoint = 0;
 float z = 0;
 
 float yaw = 0;
@@ -127,6 +131,12 @@ rsla_interfaces__msg__PoseWithMask pose_setpoint_msg;
 rcl_subscription_t wrench_setpoint_subscriber;
 rsla_interfaces__msg__WrenchWithMask wrench_setpoint_msg;
 
+rcl_subscription_t quaternion_subscriber;
+geometry_msgs__msg__Quaternion quaternion_msg;
+
+rcl_subscription_t dvl_subscriber;
+geometry_msgs__msg__Point dvl_msg;
+
 // Tuning subscriber
 rcl_subscription_t pid_tuning_subscriber;
 rsla_interfaces__msg__PID pid_tuning_msg;
@@ -154,10 +164,6 @@ rcl_node_t node;
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
 // Sensor objects
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire);
-imu::Vector<3> bno_euler_data;
-uint8_t bno_sys_cal, bno_gyro_cal, bno_acc_cal, bno_mag_cal = 0;
-
 MS5837 baro(0x76, &Wire);
 float baro_depth_data = 0;
 float surface_offset = 0;
@@ -175,8 +181,8 @@ enum class ControllerMode : uint8_t
   POSITION = 1
 };
 
-ControllerMode x_mode = ControllerMode::EFFORT;
-ControllerMode y_mode = ControllerMode::EFFORT;
+ControllerMode x_mode = ControllerMode::POSITION;
+ControllerMode y_mode = ControllerMode::POSITION;
 ControllerMode z_mode = ControllerMode::POSITION;
 ControllerMode roll_mode = ControllerMode::POSITION;
 ControllerMode pitch_mode = ControllerMode::POSITION;
@@ -225,13 +231,14 @@ void wrench_setpoint_callback(const void *msgin);
 void pid_tuning_callback(const void *msgin);
 void sw_arm_callback(const void *msgin);
 void diagnostic_command_callback(const void *msgin);
+void quaternion_callback(const void* msgin);
+void dvl_callback(const void* msgin);
 bool create_entities();
 void destroy_entities();
 void initialize_sensors();
 void initialize_controllers();
 void initialize_message_data();
 void setup();
-void get_imu();
 void get_baro();
 void get_battery();
 void update_pids(float dt);
@@ -261,8 +268,7 @@ void loop() {
     if(loop_time_millis > last_sensor_update_ms + sensor_update_interval_ms)
     {
       last_sensor_update_ms = loop_time_millis;
-
-      get_imu();
+    
       get_baro();
       get_battery();
 
@@ -336,7 +342,7 @@ void setup() {
   digitalWrite(LED_PIN, HIGH);
 
   delay(1000);
-  
+
   initialize_message_data();
 
   // Initial state
@@ -400,6 +406,8 @@ void orientation_callback(rcl_timer_t* timer, int64_t last_call_time)
     orientation_msg.orientation.pitch = pitch;
     orientation_msg.orientation.roll = roll;
 
+    orientation_msg.position.x = x;
+    orientation_msg.position.y = y;
     orientation_msg.position.z = z;
 
     RCSOFTCHECK(rcl_publish(&orientation_publisher, &orientation_msg, NULL));
@@ -424,10 +432,10 @@ void telemetry_callback(rcl_timer_t* timer, int64_t last_call_time)
     memcpy(&telemetry_msg.thruster_vector, &thruster_forces, 8 * sizeof(float));
     memcpy(&telemetry_msg.pwm_output, &thruster_pwm, 8 * sizeof(uint16_t));
 
-    telemetry_msg.imu_calibration[0] = bno_sys_cal;
-    telemetry_msg.imu_calibration[1] = bno_gyro_cal;
-    telemetry_msg.imu_calibration[2] = bno_acc_cal;
-    telemetry_msg.imu_calibration[3] = bno_mag_cal;
+    telemetry_msg.imu_calibration[0] = 0;
+    telemetry_msg.imu_calibration[1] = 0;
+    telemetry_msg.imu_calibration[2] = 0;
+    telemetry_msg.imu_calibration[3] = 0;
 
     RCSOFTCHECK(rcl_publish(&telemetry_publisher, &telemetry_msg, NULL));
   }
@@ -467,13 +475,13 @@ void pose_setpoint_callback(const void *msgin)
   if(msg->mask & 1)
   {
     x_mode = ControllerMode::POSITION;
-    x_controller.setpoint = msg->cmd.position.x;
+    x_setpoint = msg->cmd.position.x;
   }
   // Y controller
   if(msg->mask & 2)
   {
     y_mode = ControllerMode::POSITION;
-    y_controller.setpoint = msg->cmd.position.y;
+    y_setpoint = msg->cmd.position.y;
   }
   // Z controller
   if(msg->mask & 4)
@@ -642,6 +650,39 @@ void diagnostic_command_callback(const void *msgin)
   }
 }
 
+
+// DVL callback
+void dvl_callback(const void* msgin)
+{
+  const geometry_msgs__msg__Point* msg = (const geometry_msgs__msg__Point*)msgin;
+  x = msg->x;
+  y = msg->y;
+}
+
+// AHRS quaternion callback
+void quaternion_callback(const void* msgin)
+{
+  const geometry_msgs__msg__Quaternion* q = (const geometry_msgs__msg__Quaternion*)msgin;
+
+  // Conversion to Euler Angles from Quaternion
+  // roll (x-axis rotation)
+  double sinr_cosp = 2 * (q->w * q->x + q->y * q->z);
+  double cosr_cosp = 1 - 2 * (q->x * q->x + q->y * q->y);
+  roll = atan2(sinr_cosp, cosr_cosp) * 180 / M_PI;
+
+  // pitch (y-axis rotation)
+  double sinp = 2 * (q->w * q->y - q->z * q->x);
+  if (abs(sinp) >= 1)
+      pitch = copysign(90, sinp); // use 90 degrees if out of range
+  else
+      pitch = asin(sinp) * 180 / M_PI;
+
+  // yaw (z-axis rotation)
+  double siny_cosp = 2 * (q->w * q->z + q->x * q->y);
+  double cosy_cosp = 1 - 2 * (q->y * q->y + q->z * q->z);
+  yaw = atan2(siny_cosp, cosy_cosp) * 180 / M_PI;
+}
+
 // Create ROS entities
 bool create_entities()
 {
@@ -670,6 +711,10 @@ bool create_entities()
   RCCHECK(rclc_subscription_init_default(&pose_setpoint_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(rsla_interfaces, msg, PoseWithMask), "rsla/autonomy/pose_setpoint_with_mask"));
   RCCHECK(rclc_subscription_init_default(&wrench_setpoint_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(rsla_interfaces, msg, WrenchWithMask), "rsla/autonomy/wrench_setpoint_with_mask"));
 
+  // Create AHRS and DVL subscribers
+  RCCHECK(rclc_subscription_init_default(&quaternion_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Quaternion), "/ahrs/quaternion"));
+  RCCHECK(rclc_subscription_init_default(&dvl_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Point), "/dvl/point"));
+
   // Create tuning subscriber
   RCCHECK(rclc_subscription_init_default(&pid_tuning_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(rsla_interfaces, msg, PID), "rsla/controls/pid_controller_gains"));
 
@@ -683,7 +728,7 @@ bool create_entities()
   RCCHECK(rclc_subscription_init_default(&diagnostic_command_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8), "rsla/controls/diagnostic_command"));
 
   // Create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 10, &allocator));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 12, &allocator));
 
   // Add timers to executor
   RCCHECK(rclc_executor_add_timer(&executor, &status_timer));
@@ -694,6 +739,8 @@ bool create_entities()
   // Add subscribers to executor
   RCCHECK(rclc_executor_add_subscription(&executor, &pose_setpoint_subscriber, &pose_setpoint_msg, &pose_setpoint_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &wrench_setpoint_subscriber, &wrench_setpoint_msg, &wrench_setpoint_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &quaternion_subscriber, &quaternion_msg, &quaternion_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &dvl_subscriber, &dvl_msg, &dvl_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &pid_tuning_subscriber, &pid_tuning_msg, &pid_tuning_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sw_arm_subscriber, &sw_arm_msg, &sw_arm_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &diagnostic_command_subscriber, &diagmonst_command_msg, &diagnostic_command_callback, ON_NEW_DATA));
@@ -718,6 +765,8 @@ void destroy_entities()
 
   RCCHECK(rcl_subscription_fini(&pose_setpoint_subscriber, &node));
   RCCHECK(rcl_subscription_fini(&wrench_setpoint_subscriber, &node));
+  RCCHECK(rcl_subscription_fini(&quaternion_subscriber, &node));
+  RCCHECK(rcl_subscription_fini(&dvl_subscriber, &node));
   RCCHECK(rcl_subscription_fini(&pid_tuning_subscriber, &node));
   RCCHECK(rcl_subscription_fini(&sw_arm_subscriber, &node));
   RCCHECK(rcl_subscription_fini(&diagnostic_command_subscriber, &node));
@@ -736,17 +785,6 @@ void initialize_sensors() {
   else
   {
     baro.setFluidDensity(997);
-  }
-
-  if(!bno.begin())
-  {
-    vehicle_state = VehicleState::IMU_ERROR;
-    startup_successful = false;
-  }
-  else
-  {
-    bno.setMode(OPERATION_MODE_IMUPLUS);
-    bno.setExtCrystalUse(true);
   }
 
   delay(100);
@@ -789,6 +827,24 @@ void initialize_controllers() {
   pitch_controller.derivativeMode = RSLA::DerivativeMode::DERIVATIVE_ON_MEASUREMENT;
   yaw_controller.derivativeMode = RSLA::DerivativeMode::DERIVATIVE_ON_MEASUREMENT;
 
+  x_controller.kP = 30.0;
+  x_controller.kI = 1.0;
+  x_controller.kD = 10.0;
+  x_controller.antiwindup = 2.0;
+  x_controller.enableAntiwindup = true;
+  x_controller.bias = 0.0;
+  x_controller.constraint = 15.0;
+  x_controller.enableConstraint = true;
+
+  y_controller.kP = 30.0;
+  y_controller.kI = 1.0;
+  y_controller.kD = 10.0;
+  y_controller.antiwindup = 2.0;
+  y_controller.enableAntiwindup = true;
+  y_controller.bias = 0.0;
+  y_controller.constraint = 15.0;
+  y_controller.enableConstraint = true;
+
   z_controller.kP = 30.0;
   z_controller.kI = 1.0;
   z_controller.kD = 10.0;
@@ -829,14 +885,6 @@ void initialize_message_data() {
   orientation_msg.position.z = 0;
 }
 
-void get_imu() {
-      bno_euler_data = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-      yaw = bno_euler_data.x();
-      pitch = bno_euler_data.y();
-      roll = -bno_euler_data.z();
-      bno.getCalibration(&bno_sys_cal, &bno_gyro_cal, &bno_acc_cal, &bno_mag_cal);
-}
-
 void get_baro() {
   baro.read();
   baro_depth_data = baro.depth();
@@ -855,8 +903,11 @@ void update_pids(float dt) {
   yaw_controller.update(yaw, dt);
   pitch_controller.update(pitch, dt);
   roll_controller.update(roll, dt);
-  x_controller.update(x, dt);
-  y_controller.update(y, dt);
+  float theta = yaw * M_PI / 180;
+  float delta_x = x - x_setpoint;
+  float delta_y = y - y_setpoint;
+  x_controller.update(delta_x*cos(theta) + delta_y*sin(theta), dt);
+  y_controller.update(-delta_x*sin(theta) + delta_y*cos(theta), dt);
   z_controller.update(z, dt);
 }
 
